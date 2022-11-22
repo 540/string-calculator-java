1d56f9a058ef948945374175f1118bd9a75d3ae8
Iker Mariñelarena <iker@540deg.com>
2022-11-23 00:11:42 +0100

    Initial commit

---
+++ src/main/java/com/deg540/tdd/Calculator.java
@@ -0,0 +1,11 @@
+package com.deg540.tdd;
+
+class Calculator {
+    public int add(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int multiply(int number1, int number2) {
+        return number1 * number2;
+    }
+}
